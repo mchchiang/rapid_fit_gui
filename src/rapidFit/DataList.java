df9d8777af57f8b8d76c838cbfe1157dee552fa6
mchchiang <michael_chiang_ch@hotmail.com>
2015-06-04 13:50:53 +0100

    delete unused code in DataList.java

--- src/rapidFit/DataList.java
+++ src/rapidFit/DataList.java
@@ -1,20 +1,13 @@
 package rapidFit;
 
 import java.awt.*;
-import java.lang.reflect.*;
+
 import java.util.*;
 
 import javax.swing.*;
 
 @SuppressWarnings("serial")
 public class DataList<T> extends JList<T> {
-	
-	/*private Method getMethod = null;
-	
-	//only used for tag name
-	private int attrCount = 1;
-	private boolean useTagName = false;
-	private String tagName;*/
 	
 	private IdentityHashMap<T, String> nameMap = null;
 	
@@ -32,15 +25,6 @@ public class DataList<T> extends JList<T> {
 			} else {
 				setText(value.toString());
 			}
-			/*if (!useTagName){
-				try {
-					setText((String) getMethod.invoke(value, (Object []) null));
-				} catch (Exception e){
-					e.printStackTrace();
-				}
-			} else {
-				setText(nameMap.get(value));
-			}*/
 			return this;
 		}
 	}
@@ -64,81 +48,5 @@ public class DataList<T> extends JList<T> {
 	public void updateNameMap(IdentityHashMap<T, String> map){
 		nameMap = map;
 	}
-	
-	/*
-	//=====================================================================================
-	public DataList(DataListModel<T> model, Method getter){
-		super(model);
-		this.getMethod = getter;
-		this.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		setCellRenderer(new DataListRenderer());
-	}
-	
-	public DataList(DataListModel<T> model, String name, boolean useTag){
-		super(model);
-		
-		nameMap = new IdentityHashMap<T, String>();
-		nameCount = new HashMap<String, Integer>();
-		
-		if (!useTag){
-			useTagName = false;
-			
-			try {
-				//find the get method 
-				this.getMethod = model.getDataClass().getMethod("get" + name, (Class<?>[]) null);
-				
-				//create distinct name for each entry
-				for (T entry : model.getData()){
-					String attrName = getMethod.invoke(entry, (Object []) null).toString();
-					if (nameCount.containsKey(attrName)){
-						nameCount.put(attrName, nameCount.get(attrName)+1);
-					} else {
-						nameCount.put(attrName, 1);
-					}
-					nameMap.put(entry, attrName + "_" + nameCount.get(attrName));
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		} else {
-			useTagName = true;
-			tagName = name;
-			
-			for (T entry : model.getData()){
-				nameMap.put(entry, name + "_" + attrCount);
-				attrCount++;
-			}
-		}
-		this.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		setCellRenderer(new DataListRenderer());
-	}
-	
-	public void setDefaultOptions(){
-		setCellRenderer(new DataListRenderer());
-		setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-	}
-	
-	public void setTagName(T entry, String name){
-		if (useTagName){
-			nameMap.put(entry, name);
-		}
-		setCellRenderer(new DataListRenderer());
-	}
-	
-	public void setTagName(T entry){
-		if (useTagName){
-			nameMap.put(entry, tagName + "_" + attrCount);
-			attrCount++;
-		}
-		setCellRenderer(new DataListRenderer());
-	}
-	
-	/*public String getTagName(T entry){
-		if (useTagName){
-			return nameMap.get(entry);
-		} else {
-			return "NO TAG NAME";
-		}
-	}*/
 	
 }
